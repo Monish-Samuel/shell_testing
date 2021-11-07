234519da6ca04599ee60319614bf22f6fb8b8946
Monish-Samuel <79988944+Monish-Samuel@users.noreply.github.com>
2021-11-07 16:09:12 +0530

    Update unzip_deploy.sh

--- build_scripts/unzip_deploy.sh
+++ build_scripts/unzip_deploy.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 DIR= "$WORKSPACE/artifacts"
-if [-d "$DIR"]
+if [! -d "$DIR"]
 then
-  echo "- Clearing directory"
-  rm /DIR/*
-fi  
-else
   echo "- Specified directory doesn't exist"
   echo "- Creating Directory"
   mkdir artifacts
+else
+  echo "- Clearing directory"
+  rm /DIR/* 
 fi
 unzip myapp-$buildVersion.zip "$WORKSPACE/artifacts"
