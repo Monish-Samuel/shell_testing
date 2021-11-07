b50be3d9fe659b4707e7e7e9a6b14c92908b03f4
Monish-Samuel <79988944+Monish-Samuel@users.noreply.github.com>
2021-11-07 14:41:41 +0530

    Update zip_creation.sh

--- build_scripts/zip_creation.sh
+++ build_scripts/zip_creation.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-DIR="*.zip"
-if [ find . -name "*.zip" -type f ]; then
 echo "Deleting old zip file"
 find . -name "*.zip" -type f -delete
-fi
-
+sleep 3
 zip -r myapp-$BUILD_NUMBER.zip src
