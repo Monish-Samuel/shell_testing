cec1113bef460469cbe2d9cddb82630f770ba05e
Monish-Samuel <79988944+Monish-Samuel@users.noreply.github.com>
2021-11-07 14:48:28 +0530

    Update zip_creation.sh

--- build_scripts/zip_creation.sh
+++ build_scripts/zip_creation.sh
@@ -3,7 +3,7 @@ echo "- Deleting old zip file"
 echo ""
 echo ""
 find . -name "*.zip" -type f -delete
-sleep 3
+sleep 5
 echo "- Creating new zip file"
 zip -r myapp-$BUILD_NUMBER.zip src
-slepp 3
+sleep 5
